11590b9f021f3c7d10197d61fd3137c0de34ff3b
ArifAhmed <arif15-12301@diu.edu.bd>
2022-04-08 12:44:37 +0600

    second_commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Doctor;
+use App\Models\Appointment;
 
 class HomeController extends Controller
 {
@@ -37,5 +38,24 @@ class HomeController extends Controller
 
         }
         
+    }
+    public function appointment(Request $request){
+        $data = new appointment;
+        $data->name=$request->name;
+        $data->email=$request->email;
+        $data->date=$request->date;
+        $data->phone=$request->number;
+        $data->message=$request->message;
+        $data->doctor=$request->doctor;
+        $data->status='In progress';
+        if(Auth::id())
+        {
+            $data->user_id=Auth::user()->id;
+        }
+        $data->save();
+        return redirect()->back()->with('message','Appointment Request Successful.
+         We will contact with you soon');
+        
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,5 @@ Route::middleware([
 Route::get('/add_doctor_view',[AdminController::class,'addview']);
 
 Route::post('/upload_doctor',[AdminController::class,'upload']);
+
+Route::post('/appointment',[HomeController::class,'appointment']);
