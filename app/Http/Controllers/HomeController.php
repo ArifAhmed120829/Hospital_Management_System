82291b12b7f73398c01495cb8849cd795bb98aa4
ArifAhmed <arif15-12301@diu.edu.bd>
2022-04-08 11:16:28 +0600

    first_push

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Doctor;
+
+class HomeController extends Controller
+{
+    public function redirect()
+    {
+        if(Auth::id()){
+            if(Auth::user()->usertype=='0')
+            {
+                $doctor = doctor::all();
+                return view('user.home',compact('doctor'));
+
+            }
+            else{
+                return view('admin.home');
+            }
+
+        }
+        else{
+            return redirect()->back();
+        }
+    }
+    public function index(){
+        if(Auth::id()){
+            return redirect('home');
+        }
+        else{
+            $doctor = doctor::all();
+        return view('user.home',compact('doctor'));
+
+        }
+        
+    }
+}
